dd368650eac053889d4bf3ee540169aba65ca033
NotHilal <hilal.elayoubi@gmail.com>
2025-01-14 14:52:22 +0100

    Start of project

---
+++ project.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+num =[0,1,2,3,4,5,6,7,8,9]
+mini=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+cap= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+
+def getArt(a):
+    
+    
+    return a
+
+
+
+
