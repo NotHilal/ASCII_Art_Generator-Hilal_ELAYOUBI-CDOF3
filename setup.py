3e19f17cfe4cd4e3f3a6b0e6f16d41cebd382214
NotHilal <hilal.elayoubi@gmail.com>
2025-01-14 14:47:35 +0100

    Setup file updated

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from distutils.core import setup
 
 setup(
-name='{name-of-your-project}',
+name='{ASCII Art Generator}',
 version='1.0',
-author='{your-full-name-or-your-github-username}',
+author='{NotHilal}',
 license='MIT',
 long_description=open('README.md').read(),
 )
